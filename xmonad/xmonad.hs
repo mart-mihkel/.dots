8e151857f2d6a8540108a8444e97edfd82d17b3f
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-08-10 15:06:16 +0300

    xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,139 +1,61 @@
-import Data.List (sortBy)
-import Data.Monoid
-import Data.Function
-import Control.Monad (forM_, join)
-import System.Exit
-
 import XMonad
+
+import XMonad.Util.Themes
+import XMonad.Util.SpawnOnce
+
 import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
-import XMonad.Layout.Spiral
-import XMonad.Util.Run (safeSpawn, spawnPipe)
-import XMonad.Util.NamedWindows (getName)
 
-import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
 
--- Some basic settings
-myTerminal          = "alacritty"
-myTextEditor        = "nvim"
-myFocusFollowsMouse = True
-myClickJustFocuses  = False
-myBorderWidth       = 2
-mySpacing           = 4
-myGaps              = [(U, 30), (D, 10), (L, 10), (R, 10)]
-myModMask           = mod4Mask
-myWorkspaces        = map show [1..9]
+color0, color1 :: String
+color0 = "#8a999e"
+color1 = "#545d75"
 
--- color0 and color12 are declared at the end of
--- the file and are inserted by pywal
-myNormalBorderColor  = color0
-myFocusedBorderColor = color12
-
--- Keybindings
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-    , ((modm,               xK_p     ), spawn "dmenu_run")
-    , ((modm .|. shiftMask, xK_c     ), kill)
-    , ((modm,               xK_space ), sendMessage NextLayout)
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-    , ((modm,               xK_n     ), refresh)
-    , ((modm,               xK_Tab   ), windows W.focusDown)
-    , ((modm,               xK_j     ), windows W.focusDown)
-    , ((modm,               xK_k     ), windows W.focusUp  )
-    , ((modm,               xK_m     ), windows W.focusMaster  )
-    , ((modm,               xK_Return), windows W.swapMaster)
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
-    , ((modm,               xK_h     ), sendMessage Shrink)
-    , ((modm,               xK_l     ), sendMessage Expand)
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-    , ((modm .|. shiftMask, xK_q     ), io exitSuccess)
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-    ]
-    ++
-    [((m .|. modm, k), windows $ f i)
-        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-
-
-myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList
-    -- Move the window
-    [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
-                                      >> windows W.shiftMaster)
-    -- Promote window as a master
-    , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
-    -- Resize the window
-    , ((modm, button3), \w -> focus w >> mouseResizeWindow w
-                                      >> windows W.shiftMaster)
-    ]
-
--- Layouts configuration
-myLayout = tiled ||| Mirror tiled ||| Full ||| spiral (6/7) &
-               gaps myGaps &
-               smartSpacing mySpacing
+myLayout = smartSpacing 8 $ gaps [(U, 16), (D, 16), (L, 16), (R, 16)] $ avoidStruts $ tiled
   where
-     tiled   = Tall nmaster delta ratio
-     nmaster = 1
-     ratio   = 1/2
-     delta   = 3/100
+    tiled = Tall 1 (1/2) (3/100)
 
--- Window rules
+myManageHook :: ManageHook
 myManageHook = composeAll
-    [ className =? "MPlayer"        --> doFloat
-    , className =? "Gimp"           --> doFloat
-    , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore ]
+  [ className =? "MPlayer" --> doFloat
+  , resource  =? "desktop_window" --> doIgnore
+  , resource  =? "kdesktop" --> doIgnore ]
 
--- Logging for xmobar
-myEventHook = mempty
-myLogHook = do
-  winset <- gets windowset
-  title <- maybe (return "") (fmap show . getName) . W.peek $ winset
-  let currWs = W.currentTag winset
-  let wss = map W.tag $ W.workspaces winset
-  let wsStr = join $ map (fmt currWs) $ sort' wss
-
-  io $ appendFile "/tmp/.xmonad-title-log" (title ++ "\n")
-  io $ appendFile "/tmp/.xmonad-workspace-log" (wsStr ++ "\n")
-
-  where fmt currWs ws
-          -- #C15A45 font color for the current workspace
-          | currWs == ws = "<fc=#C15A45> " ++ ws ++ " </fc>"
-          | otherwise    = " " ++ ws ++ " "
-        sort' = sortBy (compare `on` (!! 0))
-
-myStartupHook = return ()
-
-main = do
-  forM_ [".xmonad-workspace-log", ".xmonad-title-log"] $ \file -> do
-    xmporc <- spawnPipe "xmobar -x 0 ~/.config/xmonad/xmobarrc"
-    safeSpawn "mkfifo" ["/tmp/" ++ file]
-    xmonad defaults
-
-defaults = def {
-        terminal           = myTerminal,
-        focusFollowsMouse  = myFocusFollowsMouse,
-        clickJustFocuses   = myClickJustFocuses,
-        borderWidth        = myBorderWidth,
-        modMask            = myModMask,
-        workspaces         = myWorkspaces,
-        normalBorderColor  = myNormalBorderColor,
-        focusedBorderColor = myFocusedBorderColor,
-
-        keys               = myKeys,
-        mouseBindings      = myMouseBindings,
-
-        layoutHook         = myLayout,
-        manageHook         = myManageHook,
-        handleEventHook    = myEventHook,
-        logHook            = myLogHook,
-        startupHook        = myStartupHook
+myMobarPP :: PP
+myMobarPP = def
+    { ppCurrent         = xmobarColor color0 "" . wrap "[" "]"
+    , ppHidden          = xmobarColor color1 ""
+    , ppHiddenNoWindows = const ""
+    , ppOrder           = \[ws, _, _] -> [ws]
     }
 
--- Inserted by cat .cache/wal/colors.hs | grep "color0"  | tail -n1 >> .config/xmonad/xmonad.hs
---             cat .cache/wal/colors.hs | grep "color12" | tail -n1 >> .config/xmonad/xmonad.hs
-color0="#0d0e0c"
-color12="#C15A45"
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce "feh --bg-fill --no-fehbg ~/dotfiles/walls/urr.jpeg"
+  spawnOnce "picom -b"
+
+main :: IO ()
+main = xmonad
+  . ewmhFullscreen
+  . ewmh
+  . withEasySB (statusBarProp "xmobar ~/.config/xmonad/xmobarrc" (pure myMobarPP)) defToggleStrutsKey
+  $ def
+  { modMask            = mod4Mask
+  , terminal           = "alacritty"
+  , workspaces         = ["dev", "www", "misc", "4", "5", "6", "7", "8", "9"]
+  , focusFollowsMouse  = True
+  , clickJustFocuses   = False
+  , borderWidth        = 0
+  , focusedBorderColor = color0
+  , normalBorderColor  = color1
+  , handleEventHook    = mempty
+  , layoutHook         = myLayout
+  , manageHook         = myManageHook
+  , startupHook        = myStartupHook
+  }
