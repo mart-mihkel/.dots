17e2fd2c0a43d1ccb4d038fcda23a3dfc1a9c9ee
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-08-12 20:40:58 +0300

    rofi

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,37 +1,39 @@
 import XMonad
 
-import XMonad.Util.Themes
+import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
 
 import XMonad.Layout.Gaps
+import XMonad.Layout.Spiral
 import XMonad.Layout.Spacing
 
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.StatusBar
-import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
 
-myLayout = avoidStruts $ gaps [(U, 46), (D, 16), (L, 16), (R, 16)] $ smartSpacing 8 $ tiled
-  where
-    tiled = Tall 1 (1/2) (3/100)
 
-myManageHook :: ManageHook
-myManageHook = composeAll
+layout = avoidStruts $ gaps [(U, 31)] $ smartSpacing 8 $ spiral (6/7)
+
+keybinds =
+  [ ("M-p", spawn "rofi -show drun")
+  , ("M-.", spawn "rofimoji")
+  , ("M-f", spawn "firefox")
+  ]
+
+manager :: ManageHook
+manager = composeAll
   [ resource  =? "desktop_window" --> doIgnore
   , resource  =? "kdesktop" --> doIgnore
   , className =? "MPlayer" --> doFloat
   ]
 
-myStartupHook :: X ()
-myStartupHook = do
-  spawnOnce "feh --bg-fill --no-fehbg ~/dotfiles/walls/a_person_riding_a_bicycle_on_a_hill_with_sunflowers.jpeg"
+startup :: X ()
+startup = do
+  spawnOnce "feh --bg-fill --no-fehbg ~/dotfiles/walls/a_cartoon_of_a_castle.png"
   spawnOnce "eww open bar"
   spawnOnce "picom -b"
 
 main :: IO ()
-main = xmonad . docks . ewmhFullscreen . ewmh $ defaults
+main = xmonad . docks . ewmhFullscreen . ewmh $ defaults `additionalKeysP` keybinds
 
 defaults = def
   { modMask            = mod4Mask
@@ -41,7 +43,7 @@ defaults = def
   , clickJustFocuses   = False
   , borderWidth        = 0
   , handleEventHook    = mempty
-  , layoutHook         = myLayout
-  , manageHook         = myManageHook
-  , startupHook        = myStartupHook
+  , layoutHook         = layout
+  , manageHook         = manager
+  , startupHook        = startup
   }
